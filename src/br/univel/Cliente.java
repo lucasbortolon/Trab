e87cef228a1ebb2f157916374e0826aabe2d8d03
Lucas Oliveira Bortolon <lucas-bortolon@hotmail.com>
2016-04-20 21:54:10 -0300

    commit

--- src/br/univel/Cliente.java
+++ src/br/univel/Cliente.java
@@ -1,5 +1,13 @@
 package br.univel;
 
+import br.univel.EstadoCivil;
+
 public class Cliente {
 
+	private int	id;
+	private String nome;
+	private String endereco;
+	private int telefone ;
+	
+	
 }
